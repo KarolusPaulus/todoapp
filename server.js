3790074a2135479ff62bec405aabcaeccc69fd4d
KarolusPaulus <karosaarinenn@gmail.com>
2024-10-06 16:44:38 +0300

    update

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let todos = [];
+
+app.get('/', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todo', (req, res) => {
+    const todo = req.body.todo;
+    todos.push(todo);
+    res.status(201).json({ message: 'Item added', todo });
+});
+
+app.delete('/todo/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    todos = todos.filter((todo, index) => index !== id);
+    res.status(200).json({ message: 'Item deleted', todos });
+});
+
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
+});
